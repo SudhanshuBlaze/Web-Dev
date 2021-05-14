14a4c06ff096050a6f0cacaa5b1f16507fb40144
Sudhanshu Kumar <63813872+SudhanshuBlaze@users.noreply.github.com>
2021-05-14 13:41:43 +0530

    update

--- 00 Projects/Blog Website/app.js
+++ 00 Projects/Blog Website/app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const ejs = require("ejs");
+const _=require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,16 +15,53 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts=[]
+
+app.get("/", (req,res)=>{
+  res.render("home", {home:homeStartingContent, posts:posts })
+})
+
+app.get("/about",(req,res)=>{
+  res.render("about", {about:aboutContent})
+})
+
+app.get("/contact", (req,res)=>{
+  res.render("contact",{contact:contactContent})
+})
+
+app.get("/compose",(req,res)=>{
+  res.render("compose")
+})
+
+app.post("/compose",(req,res)=>{
+  const post={
+    postTitle: (req.body.postTitle),
+    postBody:req.body.postBody
+  }
+  posts.push(post)
+  res.redirect("/")
+})
 
 
+app.get("/posts/:title/", (req,res)=>{
+  // console.log(req.params.title)
+  let requestedTitle= _.lowerCase(req.params.title)
 
+  for(let i=0;i<posts.length;i++){
+    let storedTitle= _.lowerCase(posts[i].postTitle)
 
+    if(requestedTitle===storedTitle){
+      console.log("Match Found");
+      res.render("post",{
+        postTitle:posts[i].postTitle,
+        postBody: posts[i].postBody
+      })
 
-
-
-
-
-
+    }
+    else
+      console.log("Match not Found");
+  }
+})
 
 
 
