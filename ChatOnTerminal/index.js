0c2e59b44f8c2d22f30a4e259323c09ce66fc327
Sudhanshu Kumar <63813872+SudhanshuBlaze@users.noreply.github.com>
2021-06-14 18:42:04 +0530

    +

---
+++ ChatOnTerminal/index.js
@@ -0,0 +1,59 @@
+const port= process.env.PORT || 3000;
+const chalk = require('chalk');
+const express = require('express');
+const app = express();
+
+const socket = require("socket.io-client")("http://localhost:3000");
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+var id = "";
+var buffer = "";
+
+
+function chat(){
+	rl.question(chalk.magenta("» "), (answer) => {
+		buffer = `${chalk.cyan(id)} : ${chalk.bgCyan(answer)}`;
+		socket.emit("message", buffer);
+		chat();
+	});
+}
+
+socket.on('connect', () => {
+
+	rl.question(`What's your name? `, (answer) => {
+			socket.emit("message", `👤  : ${chalk.green(answer)} has joined the chat`);
+			id = answer;
+			chat();
+
+	});
+
+	socket.on('msg', function(data){
+		if(buffer!=data){
+			console.log("\n" + data);
+			chat();
+		}
+	});
+	
+})
+
+
+io.on('connection', function (socket) {
+	socket.on('message', function(data) {
+		io.emit("msg", data);
+	});
+});
+
+
+http.listen(port, function() {
+  console.log('listening on 3000');
+});
