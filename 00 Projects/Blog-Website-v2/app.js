d28ffc5b64e7754d6b7aa182c1e66f2289dfd22e
SudhanshuBlaze <skrourkela@gmail.com>
2021-08-31 23:36:30 +0530

    Blog Website v2

--- 00 Projects/Blog-Website-v2/app.js
+++ 00 Projects/Blog-Website-v2/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose= require("mongoose");
 
+connect().catch(err=>console.log(err));
+async function connect() {
+  await mongoose.connect("mongodb://localhost:27017/blogsDB", 
+  {useNewUrlParser: true,useUnifiedTopology: true });
+}
+//dummy data
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -13,13 +20,20 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let posts = [];
+const blogSchema=new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Blog= mongoose.model("Blog", blogSchema);
 
 app.get("/", function (req, res) {
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
-  });
+  Blog.find({}).then(foundBlogs=>{
+    res.render("home",{
+      startingContent: homeStartingContent,
+      posts: foundBlogs
+    })
+  }).catch(err=>console.log("Something went wrong at Home route"+err));
 });
 
 app.get("/about", function (req, res) {
@@ -35,33 +49,33 @@ app.get("/compose", function (req, res) {
 });
 
 app.post("/compose", function (req, res) {
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  };
+  const title = _.lowerCase(req.body.postTitle);
+  const content=req.body.postBody;
 
-  posts.push(post);
-
-  res.redirect("/");
-
-});
-
-app.get("/posts/:postName", function (req, res) {
-  const requestedTitle = _.lowerCase(req.params.postName);
-
-  posts.forEach(function (post) {
-    const storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
-    }
+  const newBlog= new Blog({
+    title: title,
+    content: content
   });
-
+  //so it will be redirected only after the newBlog is saved, because it takes few millis to save but
+  //our page loads before that, due to this sometimes our saved blog is shown and we need to refresh
+  newBlog.save().then(()=>{
+    res.redirect("/");
+  })
 });
 
-app.listen(3000, function () {
+app.get("/posts/:title", function (req, res) {
+
+  const requestedTitle = _.lowerCase(req.params.title);
+
+  //note: 'find()' returns an array so use 'findOne()' for one object
+  Blog.findOne({title:requestedTitle}).then(foundPost=>{
+    res.render("post",{
+      title: foundPost.title,
+      content:foundPost.content
+    })
+  }).catch(err=>console.log("In dynamic parameters"+err));  
+});
+
+app.listen(process.env.PORT||3000, function () {
   console.log("Server started on port 3000");
 });
