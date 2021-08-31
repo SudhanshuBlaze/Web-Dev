09b313e11ba55b3c191749b0642f34b91c717e28
SudhanshuBlaze <skrourkela@gmail.com>
2021-08-31 12:41:53 +0530

    +

--- js playground/index.js
+++ js playground/index.js
@@ -1,20 +1,20 @@
-const users=[
-    {fn:"akshay",ln:"saini", age:26},
-    {fn:"donald",ln:"trump",age:75},
-    {fn:"deepika",ln:"padukone", age:26},
-    {fn:"elon",ln:"musk", age:50}
+const users = [
+    { fn: "akshay", ln: "saini", age: 26 },
+    { fn: "donald", ln: "trump", age: 75 },
+    { fn: "deepika", ln: "padukone", age: 26 },
+    { fn: "elon", ln: "musk", age: 50 }
 ];
 
 //using reduce 
-const output1=users.reduce((acc,curr)=>{
-    if(curr.age<30){
-        acc[curr.fn]=curr.age;
+const output1 = users.reduce((acc, curr) => {
+    if (curr.age < 30) {
+        acc[curr.fn] = curr.age;
     }
     return acc;
-},{});
+}, {});
 console.log(output1);
 
 //using chaining
-const output2=users.filter(x=> x.age<30)
-.map(x=>x.fn);
+const output2 = users.filter(x => x.age < 30)
+    .map(x => x.fn);
 console.log(output2);
